f368ed4c83168e8db3ce2d6ea260c1fa4ee1d17a
SeanFuller294 <s.fuller294@hotmail.com>
2019-09-13 14:13:05 -0600

    Followers-bar? showing

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,36 +2,36 @@
   <div class="home conatainer-fluid">
     <div class="row">
       <div class="col-3">
-        <img :src="user.image" />
+        <!-- <img :src="user.image" /> -->
       </div>
       <div class="col-6">
         <netWorth />
       </div>
       <div class="col-3" id="bio">
-        <h5>{{user.name}}</h5>
+        <!-- <h5>{{user.name}}</h5>
         <h5>{{user.email}}</h5>
         <h5>{{user.location}}</h5>
         <h5>{{user.work}}</h5>
-        <h5>{{user.phone}}</h5>
+        <h5>{{user.phone}}</h5>-->
       </div>
     </div>
     <div class="row">
-      <div class="col-1">
+      <div class="col-2">
         <ul>
           <li @click="showFollowers()">Followers</li>
           <li @click="showFollowing()">Following</li>
           <li @click="createPost()">Create Post</li>
         </ul>
       </div>
-      <div class="col-11">
-        <posts @click="viewPost()" />
+      <div class="col-10">
+        <Posts @click="viewPost()" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Post from "./Post";
+import Posts from "./Post";
 
 export default {
   name: "Home",
@@ -45,4 +45,7 @@ export default {
 </script>
 
 <style>
+ul {
+  text-align: left !important;
+}
 </style>
